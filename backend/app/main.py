d31e03c967b1c97222ad26402b36fdae791dc23b
Yungxin <101496626+shinyungxin@users.noreply.github.com>
2022-12-16 01:32:10 +0800

    tested with mongoDB connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,27 @@
 from fastapi import FastAPI, Query, HTTPException
+from bson import ObjectId
+from schematics.models import Model
+from schematics.types import StringType, EmailType
+from pymongo import MongoClient
+import app.settings as settings
+
+client = MongoClient(settings.mongodb_uri, settings.port)
+db = client.testingdata
+
+class Customer(Model):
+    cust_id = ObjectId()
+    cust_email = EmailType(required=True)
+    cust_name = StringType(required=True)
+
+# an instance of User
+newuser = Customer()
+
+# function to create user
+def create_user(email, username):
+    newuser.custid = ObjectId()
+    newuser.cust_email = email
+    newuser.cust_name = username
+    return dict(newuser)
 
 app = FastAPI()
 
@@ -20,6 +43,11 @@ async def authenticate_user(username: str, password: str):
     return True
 
 
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, world!"}
+
+
 @app.post("/login")
 async def login(username: str = Query(..., description="The username of the user"),
                 password: str = Query(..., description="The password of the user")):
@@ -31,3 +59,23 @@ async def login(username: str = Query(..., description="The username of the user
     is_authenticated = await authenticate_user(username, password)
     if is_authenticated:
         return {"message": "Login successful"}
+
+
+@app.post("/signup/{email}/{username}")
+def add_user(email, username: str):
+    user_exist = False
+    data = create_user(email, username)
+
+    # convert data to dict
+    dict(data)
+
+    # check if email exist already
+    if len(list(db.users.find(
+        {'email': data['cust_email']}
+        ))) > 0:
+        user_exist = True
+        print("Customer exists")
+        return {"message" : "Customer exists"}
+    elif user_exist == False:
+        db.users.insert_one(data)
+        return {"message" : "User created", "email" : data['cust_email'], "name" : data['cust_name']}
---
+++ backend/app/settings.py
@@ -0,0 +1,3 @@
+# MongoDB attributes
+mongodb_uri = "mongodb+srv://yungxin:yungxinpassword@mochi.j3njxdv.mongodb.net/mochi?retryWrites=true&w=majority"
+port = 80
