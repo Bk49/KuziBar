28796cae83647f5244d3aad669e53832df48a045
Bk49 <ericngyongwei@gmail.com>
2022-12-24 12:48:30 +0800

    Create stories for TextButton, TextIconButton and NavBar components

--- frontend/src/stories/components/common/button/TextButton.stories.js
+++ frontend/src/stories/components/common/button/TextButton.stories.js
@@ -1,1 +1,18 @@
 // The button for ref will be Create New Lottery! button
+import React from 'react'
+
+import TextButton from '../../../../components/common/button/TextButton'
+
+export default {
+    title:"Common/Button/TextButton",
+    component: TextButton
+}
+
+const Template = args => <TextButton {...args}/>
+
+export const Primary = Template.bind({})
+
+Primary.args = {
+    color:"#F77F00",
+    text:"Login"
+}
--- frontend/src/stories/components/common/button/TextIconButton.stories.js
+++ frontend/src/stories/components/common/button/TextIconButton.stories.js
@@ -1,1 +1,18 @@
 // For Connect Wallet button that has an icon
+import React from "react";
+import TextIconButton from "../../../../components/common/button/TextIconButton";
+import { ReactComponent as WalletIcon } from "../../../../assets/icons/Wallet Filled.svg";
+
+export default {
+    title: "Common/Button/TextIconButton",
+    component: TextIconButton,
+};
+
+const Template = (args) => <TextIconButton {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+    text: "Connect Wallet",
+    Icon: WalletIcon,
+};
