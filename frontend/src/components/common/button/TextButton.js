99394c4b3bf893229b22acddddeabdf9cd6cd264
Bk49 <ericngyongwei@gmail.com>
2022-12-24 12:47:22 +0800

    Create TextButton component

--- frontend/src/components/common/button/TextButton.js
+++ frontend/src/components/common/button/TextButton.js
@@ -1,1 +1,16 @@
 // The button for ref will be Create New Lottery! button
+import "../../../assets/css/components/common/button/TextButton.css";
+
+const TextButton = ({ text, color, onClick }) => {
+    return (
+        <button
+            onClick={onClick}
+            className="button"
+            style={{ background: color }}
+        >
+            <span className="button-text">{text}</span>
+        </button>
+    );
+};
+
+export default TextButton;
