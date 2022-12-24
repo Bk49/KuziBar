aae952abf75b7597acaae7337c50df81e9bcf8f7
Bk49 <ericngyongwei@gmail.com>
2022-12-24 12:47:37 +0800

    Create TextIconButton component

--- frontend/src/components/common/button/TextIconButton.js
+++ frontend/src/components/common/button/TextIconButton.js
@@ -1,1 +1,16 @@
 // For Connect Wallet button that has an icon
+import "../../../assets/css/components/common/button/TextIconButton.css";
+
+const TextIconButton = ({ text, onClick, Icon }) => {
+    return (
+        <button
+            onClick={onClick}
+            className="button2"
+        >
+            <span className="button2-text">{text}</span>
+            <Icon className="button2-icon" />
+        </button>
+    );
+};
+
+export default TextIconButton;
