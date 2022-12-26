5a3c1d88dda93e4b3ca33d9baef68b11414f2119
Bk49 <ericngyongwei@gmail.com>
2022-12-26 20:18:05 +0800

    Create itemSlice to temporarily store item in Add Item page

---
+++ frontend/src/redux/slice/itemSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const itemSlice = createSlice({
+    name: "item",
+    initialState: {
+        item_name: "",
+        tier: 1,
+        image: "",
+        skins: [],
+    },
+    reducers: {
+        setItemVal: (currentState, { payload }) => {
+            currentState[payload.key] = payload.value;
+        },
+
+        addSkin: (currentState) => {
+            currentState.skins.push({
+                skin_name: "",
+                skin_image: "",
+            });
+        },
+
+        removeSkin: (currentState, { payload }) => {
+            currentState.skins.splice(payload.index, 1);
+        },
+
+        setSkinVal: (currentState, { payload }) => {
+            currentState.skins[payload.index][payload.key] = payload.value;
+        },
+
+        reset: (currentState) => ({
+            item_name: "",
+            tier: 1,
+            image: "",
+            skins: [],
+        }),
+    },
+});
+
+export const { setItemVal, addSkin, removeSkin, setSkinVal, reset } =
+    itemSlice.actions;
+
+export default itemSlice.reducer;
