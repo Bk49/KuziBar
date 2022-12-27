ae5e38052dc528adc39cd16af90bf8a5df61f024
Bk49 <ericngyongwei@gmail.com>
2022-12-27 14:22:19 +0800

    Reducing file size of CreateLotteryPage by creating functions outside of the file and creating CreateLotteryItemCard component

--- frontend/src/components/lottery/card/CreateLotteryItemCard.js
+++ frontend/src/components/lottery/card/CreateLotteryItemCard.js
@@ -1,1 +1,127 @@
 // This is the card being displayed after the user "Add Item" in the Create Lottery Page
+
+import { Grid, Card, Label, Header, Icon } from "semantic-ui-react";
+import { setItem } from "../../../redux/slice/itemSlice";
+import { deleteLotteryItem } from "../../../redux/slice/lotterySlice";
+import handleColor from "../../../functions/handleColor";
+
+const CreateLotteryItemCard = ({
+    index,
+    lotteryObj,
+    dropRate,
+    dispatch,
+    navigate,
+}) => {
+    return (
+        <Grid.Column style={{ marginTop: "1rem" }}>
+            <Card
+                fluid
+                color={handleColor(lotteryObj.lottery_items[index].tier)}
+            >
+                <Grid>
+                    <Grid.Row columns={2}>
+                        <Grid.Column>
+                            <img
+                                className="create-lottery-item-image-preview"
+                                src={lotteryObj.lottery_items[index].image}
+                                height="100%"
+                                width="100%"
+                                alt=""
+                            />
+                            <Label
+                                style={{
+                                    marginRight: "1rem",
+                                }}
+                                color={handleColor(
+                                    lotteryObj.lottery_items[index].tier
+                                )}
+                                attached="top right"
+                            >
+                                Tier
+                                {lotteryObj.lottery_items[index].tier}
+                            </Label>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div className="create-lottery-item-card-content-container">
+                                <Card.Content>
+                                    <div className="create-lottery-item-header-container">
+                                        <Header>
+                                            {
+                                                lotteryObj.lottery_items[index]
+                                                    .item_name
+                                            }
+                                        </Header>
+                                        <div
+                                            onClick={() => {
+                                                dispatch(
+                                                    setItem({
+                                                        ...lotteryObj
+                                                            .lottery_items[
+                                                            index
+                                                        ],
+                                                        index: index,
+                                                    })
+                                                );
+                                                navigate(
+                                                    "/create-lottery/edit-item"
+                                                );
+                                            }}
+                                        >
+                                            <Icon name="edit" />
+                                        </div>
+                                    </div>
+                                    <br />
+                                    <Card.Description>
+                                        Skins:
+                                        {lotteryObj.lottery_items[index]
+                                            .skins &&
+                                        lotteryObj.lottery_items[index].skins
+                                            .length > 0 ? (
+                                            <ul>
+                                                {lotteryObj.lottery_items[
+                                                    index
+                                                ].skins.map((skin, skinI) => (
+                                                    <li>
+                                                        {
+                                                            lotteryObj
+                                                                .lottery_items[
+                                                                index
+                                                            ].skins[skinI]
+                                                                .skin_name
+                                                        }
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        ) : (
+                                            <>
+                                                {" No Skins"}
+                                                <br />
+                                            </>
+                                        )}
+                                        {`Drop Rate: ${
+                                            dropRate[
+                                                lotteryObj.lottery_items[index]
+                                                    .tier - 1
+                                            ]
+                                        }%`}
+                                    </Card.Description>
+                                </Card.Content>
+                                <div className="create-lottery-item-delete-icon-container">
+                                    <div
+                                        onClick={() =>
+                                            dispatch(deleteLotteryItem(index))
+                                        }
+                                    >
+                                        <Icon name="trash" />
+                                    </div>
+                                </div>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Card>
+        </Grid.Column>
+    );
+};
+
+export default CreateLotteryItemCard;
--- frontend/src/pages/lottery/CreateLotteryPage.js
+++ frontend/src/pages/lottery/CreateLotteryPage.js
@@ -5,29 +5,12 @@ import ImageInput from "../../components/common/input/ImageInput";
 import TextInput from "../../components/common/input/TextInput";
 import { useSelector, useDispatch } from "react-redux";
 import { setLotteryVal } from "../../redux/slice/lotterySlice";
-import { Card, Form, Grid, Header, Icon, Label } from "semantic-ui-react";
+import { Form, Grid, Header } from "semantic-ui-react";
 import { useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 import "../../assets/css/pages/lottery/CreateLotteryPage.css";
-
-const handleColor = (tier) => {
-    switch (tier) {
-        case "1":
-            return "red";
-        case "2":
-            return "orange";
-        case "3":
-            return "yellow";
-        case "4":
-            return "purple";
-        case "5":
-            return "blue";
-        case "6":
-            return "green";
-        default:
-            return "grey";
-    }
-};
+import CreateLotteryItemCard from "../../components/lottery/card/CreateLotteryItemCard";
+import calcDropRate from "../../functions/calcDropRate";
 
 const CreateLotteryPage = () => {
     const lotteryObj = useSelector((state) => state.lottery);
@@ -38,25 +21,7 @@ const CreateLotteryPage = () => {
     ]);
 
     useEffect(() => {
-        console.log(lotteryObj);
-        if (lotteryObj.lottery_items[0] && lotteryObj.lottery_items[0].image)
-            console.log(lotteryObj.lottery_items[0].image);
-    }, [lotteryObj]);
-
-    useEffect(() => {
-        let tierCount = [0, 0, 0, 0, 0, 0, 0];
-        for (let { tier } of lotteryObj.lottery_items) {
-            tierCount[tier - 1] = tierCount[tier - 1] + 1;
-        }
-        setDropRate([
-            5.0 / tierCount[0],
-            8.0 / tierCount[1],
-            12.0 / tierCount[2],
-            15.0 / tierCount[3],
-            18.0 / tierCount[4],
-            20.0 / tierCount[5],
-            22.0 / tierCount[6],
-        ]);
+        setDropRate(calcDropRate(lotteryObj.lottery_items));
     }, [lotteryObj.lottery_items]);
 
     return (
@@ -136,129 +101,7 @@ const CreateLotteryPage = () => {
                         <Grid.Row columns={2}>
                             {lotteryObj.lottery_items.length > 0 ? (
                                 lotteryObj.lottery_items.map((item, index) => (
-                                    <Grid.Column>
-                                        <Card
-                                            fluid
-                                            color={handleColor(
-                                                lotteryObj.lottery_items[index]
-                                                    .tier
-                                            )}
-                                        >
-                                            <Grid>
-                                                <Grid.Row columns={2}>
-                                                    <Grid.Column>
-                                                        <img
-                                                            className="create-lottery-item-image-preview"
-                                                            src={
-                                                                lotteryObj
-                                                                    .lottery_items[
-                                                                    index
-                                                                ].image
-                                                            }
-                                                            height="100%"
-                                                            width="100%"
-                                                            alt=""
-                                                        />
-                                                        <Label
-                                                            style={{
-                                                                marginRight:
-                                                                    "1rem",
-                                                            }}
-                                                            color={handleColor(
-                                                                lotteryObj
-                                                                    .lottery_items[
-                                                                    index
-                                                                ].tier
-                                                            )}
-                                                            attached="top right"
-                                                        >
-                                                            Tier
-                                                            {
-                                                                lotteryObj
-                                                                    .lottery_items[
-                                                                    index
-                                                                ].tier
-                                                            }
-                                                        </Label>
-                                                    </Grid.Column>
-                                                    <Grid.Column>
-                                                        <div className="create-lottery-item-card-content-container">
-                                                            <Card.Content>
-                                                                <div className="create-lottery-item-header-container">
-                                                                    <Header>
-                                                                        {
-                                                                            lotteryObj
-                                                                                .lottery_items[
-                                                                                index
-                                                                            ]
-                                                                                .item_name
-                                                                        }
-                                                                    </Header>
-                                                                    <Icon name="edit" />
-                                                                </div>
-                                                                <br />
-                                                                <Card.Description>
-                                                                    Skins:
-                                                                    {lotteryObj
-                                                                        .lottery_items[
-                                                                        index
-                                                                    ].skins &&
-                                                                    lotteryObj
-                                                                        .lottery_items[
-                                                                        index
-                                                                    ].skins
-                                                                        .length >
-                                                                        0 ? (
-                                                                        <ul>
-                                                                            {lotteryObj.lottery_items[
-                                                                                index
-                                                                            ].skins.map(
-                                                                                (
-                                                                                    skin,
-                                                                                    skinI
-                                                                                ) => (
-                                                                                    <li>
-                                                                                        {
-                                                                                            lotteryObj
-                                                                                                .lottery_items[
-                                                                                                index
-                                                                                            ]
-                                                                                                .skins[
-                                                                                                skinI
-                                                                                            ]
-                                                                                                .skin_name
-                                                                                        }
-                                                                                    </li>
-                                                                                )
-                                                                            )}
-                                                                        </ul>
-                                                                    ) : (
-                                                                        <span>
-                                                                            No
-                                                                            Skins
-                                                                        </span>
-                                                                    )}
-                                                                    {`Drop Rate: ${
-                                                                        dropRate[
-                                                                            lotteryObj
-                                                                                .lottery_items[
-                                                                                index
-                                                                            ]
-                                                                                .tier -
-                                                                                1
-                                                                        ]
-                                                                    }%`}
-                                                                </Card.Description>
-                                                            </Card.Content>
-                                                            <div className="create-lottery-item-delete-icon-container">
-                                                                <Icon name="trash" />
-                                                            </div>
-                                                        </div>
-                                                    </Grid.Column>
-                                                </Grid.Row>
-                                            </Grid>
-                                        </Card>
-                                    </Grid.Column>
+                                    <CreateLotteryItemCard index={index} lotteryObj={lotteryObj} dropRate={dropRate} dispatch={dispatch} navigate={navigate} />
                                 ))
                             ) : (
                                 <Header size="medium">No items</Header>
