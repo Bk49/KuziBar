2c4ab0283646c6e43c356018af91e1b3c2a82808
Bk49 <ericngyongwei@gmail.com>
2022-12-28 11:30:11 +0800

    Working on this file to sign the JWT correctly to the redux state

--- frontend/src/redux/slice/userSlice.js
+++ frontend/src/redux/slice/userSlice.js
@@ -1,16 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import jwt from "jsonwebtoken"
 
 export const userSlice = createSlice({
     name: "user",
     initialState: {
         email: "",
         name: "",
+        token: "",
     },
     reducers: {
-        setVal: (currentState, { payload }) => currentState.payload.key = payload.value,
+        setVal: (currentState, { payload }) =>
+            (currentState.payload.key = payload.value),
+
+        decodeToken: (currentState, { payload }) => {
+            // This reducer func is supposed to decrypt the token and set it into the state
+            // Later on, we will use axios.interceptor to append the header before the request
+            //  is being sent to the back end for APIs that are not opened to public
+        },
     },
 });
 
-export const { setVal } = userSlice.actions
+export const { setVal, decodeToken } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
