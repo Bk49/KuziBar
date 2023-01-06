e683f8411cddcccb7cc675799c5f6a9c2acb8c59
Bk49 <ericngyongwei@gmail.com>
2023-01-06 18:29:05 +0800

    Separate and complete log in and register account in the app

---
+++ frontend/src/axios/authAPI.js
@@ -0,0 +1,53 @@
+import instance, { authInstance } from "./config";
+
+const login = async ({ email, password }) => {
+    const formData = new FormData();
+    formData.append("username", email);
+    formData.append("password", password);
+
+    try {
+        const res = await instance.post(`/token/`, formData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                accept: "application/json",
+            },
+        });
+
+        return res;
+    } catch (e) {
+        throw e;
+    }
+};
+
+const getUserId = async () => {
+    try {
+        return await authInstance.get(
+            `/users/me`,
+            {},
+            {
+                headers: {
+                    withCredentials: true,
+                    "Content-Type": "application/json",
+                    accept: "application/json",
+                },
+            }
+        );
+    } catch (e) {
+        throw e;
+    }
+};
+
+const register = async (data) => {
+    try {
+        return await instance.post(`/signup`, data, {
+            headers: {
+                "Content-Type": "application/json",
+                accept: "application/json",
+            },
+        });
+    } catch (e) {
+        throw e;
+    }
+};
+
+export { login, getUserId, register };
