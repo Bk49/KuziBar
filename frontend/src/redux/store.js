f2bc2010aa0bb307f0f3317a55175d21cf3764a4
Bk49 <ericngyongwei@gmail.com>
2023-01-06 18:32:05 +0800

    Remove auth reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./slice/userSlice";
 import lotteryReducer from "./slice/lotterySlice";
 import itemReducer from "./slice/itemSlice";
 import authReducer from "./slice/authSlice";
 
 export default configureStore({
     reducer: {
-        user: userReducer,
         lottery: lotteryReducer,
         item: itemReducer,
         auth: authReducer,
