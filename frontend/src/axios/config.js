fcd83ca824b365dc2e14a6c7d29ea39f9fcb1384
Bk49 <ericngyongwei@gmail.com>
2023-01-06 18:29:38 +0800

    Create authInstance to handle requests that requires token

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,61 +1,15 @@
 import axios from "axios";
-import {
-    selectCurrentToken,
-    selectRefreshToken,
-    setAccessToken,
-} from "../redux/slice/authSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { setCredentials } from "../redux/slice/authSlice";
 
 const instance = axios.create({
     baseURL: "http://localhost",
 });
 
-// instance.interceptors.request.use(
-//     (config) => {
-//         if (token) {
-//             config.headers["Authorization"] = "Bearer " + token;
-//         }
+const authInstance = axios.create({
+    baseURL: "http://localhost",
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+});
 
-//         return config;
-//     },
-//     (error) => {
-//         Promise.reject(error);
-//     }
-// );
-
-// instance.interceptors.response.use(
-//     (res) => {
-//         return res;
-//     },
-//     function (error) {
-//         const originalRequest = error.config;
-//         console.log(error.config);
-//         if (
-//             error.response.status === 401 &&
-//             originalRequest.url === "http://localhost/token"
-//         ) {
-//             //push to login page
-//             return Promise.reject(error);
-//         }
-
-//         if (error.response.status === 401 && !originalRequest._retry) {
-//             originalRequest._retry = true;
-//             const refreshToken = selectRefreshToken;
-//             return axios
-//                 .post("/token", {
-//                     refresh_token: refreshToken,
-//                 })
-//                 .then((res) => {
-//                     if (res.status === 201) {
-//                         useDispatch(setAccessToken(res.data));
-//                         axios.defaults.headers.common["Authorization"] =
-//                             "Bearer " + selectCurrentToken;
-//                         return axios(originalRequest);
-//                     }
-//                 });
-//         }
-//         return Promise.reject(error);
-//     }
-// );
 export default instance;
+export { authInstance };
