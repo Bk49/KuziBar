ebfc5f0f92aea91a8eddedc0dae10339dd114e4b
Bk49 <ericngyongwei@gmail.com>
2023-01-07 22:05:22 +0800

    Create API to buy lottey

---
+++ frontend/src/axios/ticketAPI.js
@@ -0,0 +1,27 @@
+import { authInstance } from "./config";
+
+const buyTicket = async (lotteryId, quantity) => {
+    try {
+        const userId = localStorage.getItem("userId");
+        if (!userId) throw "userId is not set in the localStorage";
+        return await authInstance.post(
+            `/ticket`,
+            {
+                lottery_id: lotteryId,
+                user_id: userId,
+                entry_quantity: quantity,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    accept: "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            }
+        );
+    } catch (e) {
+        throw e;
+    }
+};
+
+export { buyTicket };
